4ec8d59c74c5a91f6666d5ca3de6ff3bf70ce141
hennaheinonen <henna.heinonen96@gmail.com>
2018-04-02 19:47:41 +0300

    Bban
    Bban

---
+++ Bban/Bban.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Bban
+{
+    class Bban
+    {
+        public string Account { get; set; }
+        public bool IsMachineNumber { get; set; }
+        public string MachineNumber { get; set; }
+
+
+        public Bban(string bban)
+        {
+            Account = bban;
+            string tmpRefAccount = Account;
+            IsMachineNumber = BankUtil.CorrectNumber(ref tmpRefAccount);
+            MachineNumber = tmpRefAccount;
+        }
+
+        public bool IsValid()
+        {
+            return BankUtil.IsValidAccount(Account);
+        }
+
+        public override string ToString()
+        {
+            return $"BBAN: {Account}\n" +
+                   $"MachineNumber: {MachineNumber}\n" +
+                   $"IsValid: {IsMachineNumber}\n" +
+                   $"--------------------------\n";
+
+        }
+    }
+}
---
+++ Bban/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Bban
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Bban newAcount = new Bban("227720A35988");
+
+            Console.WriteLine(newAcount.ToString());
+
+            Bban account_2 = new Bban("123456-781");
+            Console.WriteLine(account_2.ToString());
+
+        }
+    }
+}
